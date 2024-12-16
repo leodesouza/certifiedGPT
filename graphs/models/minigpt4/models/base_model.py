fbc6e68ba51e78ad5e431d816211c7a013c92e34
Leonardo Souza <leo.desouza@gmail.com>
2024-12-16 08:49:38 -0300

    Update base_model.py

--- graphs/models/minigpt4/models/base_model.py
+++ graphs/models/minigpt4/models/base_model.py
@@ -24,6 +24,7 @@ from graphs.models.minigpt4.common.dist_utils import download_cached_file
 from graphs.models.minigpt4.common.utils import get_abs_path, is_url
 from graphs.models.minigpt4.models.eva_vit import create_eva_vit_g
 from graphs.models.minigpt4.models.modeling_llama import LlamaForCausalLM
+from common.registry import registry
 
 
 class BaseModel(nn.Module):
