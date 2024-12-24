159829c02a3afd29b7fcfacd980ec231eb40537a
Leonardo <swf_developer@outlook.com>
2024-12-24 08:16:57 -0300

    script to compute std and mean

--- utils/compute_std_mean.py
+++ utils/compute_std_mean.py
@@ -4,26 +4,31 @@ from torch.utils.data import DataLoader
 import numpy as np
 
 
-transform = transforms.Compose([transforms.Resize((224, 224)), transforms.Tensor()])
+def compute():
+    root_dir = "E:/pesquisa_ia/projetos/datasets/vqav2/images/sample/train"
+
+    transform = transforms.Compose([transforms.Resize((224, 224)), transforms.Tensor()])
+
+    num_pixels = 0
+    pixel_sum = torch.zeros(3)  # 3 channels (RGB)
+    pixels_sum_squared = torch.zeros(3)
+
+    for root, _, files in tqdm(files, desc="Processing images"):
+        if file.lower().endswidth((".jpg", ".jpeg", ".png", ".bmp")):
+            img_path = os.path.join(root, file)
+            image = Image.open(img_path).convert("RGB")
+            tensor = transform(image)
+
+            pixel_sum += tensor.sum(dim=(1, 2))
+            pixels_sum_squared += (tensor**2).sum(dim=(1, 2))
+            num_pixels += tensor.size(1) * tensor.size(2)
+
+    mean = pixel_sum / num_pixels
+    std = torch.sqrt(pixel_sum / num_pixels - mean**2)
+
+    print(f"Mean: {mean.tolist()}")
+    print(f"Std: {std.tolist()}")
 
 
-dataset = datasets.ImageFolder(root="path/to/coco_images", transform=transform)
-dataloader = DataLoader(dataset, batch_size=32, shuffle=False, num_workers=2)
-
-num_pixels = 0
-sum_channels = torch.zeros(3)  # 3 channels (RGB)
-sum_channels_squared = torch.zeros(3)
-
-
-for images, _ in dataloader:
-    pixels = images.view(images.size(0), images.size(1), -1)  # Shape: [B, C, H*W]
-    num_pixels += pixels.size(2) * images.size(0)  # Total number of pixels per channel
-
-    sum_channels += pixels.sum(dim=(0, 2)).sum(dim=0)
-    sum_channels_squared += (pixels * 2).sum(dim=(0, 2)).sum(dim=0)
-
-mean = sum_channels / num_pixels
-std = torch.sqrt(sum_channels_squared / num_pixels - mean**2)
-
-print(f"Mean: {mean}")
-print(f"Std: {std}")
+if __name__ == "__main__":
+    compute()
