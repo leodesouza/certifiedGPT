a8187aebcd9d6e73a62e65af5831122efe07f500
leonardosouza <leo.desouza@gmail.com>
2025-01-06 16:21:58 -0300

    set use_bitsandbytes=False to load LLM in TPU

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 # cd "/mnt/e/pesquisa_ia/projetos/certifiedGPT" || exit 1
 
 # Run the Python script with the specified configuration file
-python3 train.py --config-path "~/certifiedGPT/configs/train_configs/certifiedgpt_finetune1.yaml"
+python3 train.py --config-path "/home/leonardosouza/certifiedGPT/configs/train_configs/certifiedgpt_finetune1.yaml"
 
 
 # Pause equivalent (Optional: keep the terminal open to review output)
