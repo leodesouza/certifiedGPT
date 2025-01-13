0a5403591e0aeed626d7f34736348ced8aab89e2
leonardosouza <leo.desouza@gmail.com>
2025-01-13 00:33:14 -0300

    change distributed method

--- agents/minigpt4_finetune_agent.py
+++ agents/minigpt4_finetune_agent.py
@@ -182,7 +182,7 @@ class MiniGPT4FineTuneAgent(BaseAgent):
             self.lr_scheduler.step(cur_epoch=epoch, cur_step=curr_step)
 
             self.logger.info("autocast")   
-            with xla_amp.autocast(enabled=self.config.run.amp): 
+            with xla_amp.autocast(enabled=self.config.run.amp, device=self.device): 
                 outputs = self.model(batch_sample)
                 loss = outputs["loss"]
                 
