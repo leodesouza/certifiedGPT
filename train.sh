606790c5a6969adba713a102879bd6216fac6327
leonardosouza <leo.desouza@gmail.com>
2025-01-12 21:35:27 -0300

    change distributed method

--- train.sh
+++ train.sh
@@ -3,8 +3,10 @@
 NUM_TPU_CORES=8
 NOISE_LEVEL=0.25
 MAX_EPOCHS=1
-BATCH_SIZE=64
+BATCH_SIZE=24
 CHECKPOINT_NAME="checkpoint_finetune_vqav2_0.25.pth"
+CONFIG_PATH='/home/leonardosouza/certifiedGPT/configs/train_configs/certifiedgpt_finetune1.yaml'
+
 
 # Execute the Python training script
 python3 train.py \
