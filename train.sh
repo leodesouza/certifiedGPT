73f548c0a0591234facc2242fa370c369c3db3df
leonardosouza <leo.desouza@gmail.com>
2025-01-16 00:02:34 -0300

    setup for finetuning with noise

--- train.sh
+++ train.sh
@@ -2,21 +2,21 @@
 
 export PJRT_DEVICE=TPU
 
-NOISE_LEVEL=0
-MAX_EPOCHS=5
-BATCH_SIZE=0
-CHECKPOINT_NAME="minigpt4_stage2"
-# CONFIG_PATH='/home/leonardosouza/certifiedGPT/configs/train_configs/certifiedgpt_finetune1.yaml'
-CONFIG_PATH='/home/leonardosouza/certifiedGPT/configs/train_configs/cc_sbu_finetuning.yaml'
+NOISE_LEVEL=0.25
+MAX_EPOCHS=1
+BATCH_SIZE=64
+CHECKPOINT_NAME="certifiedgpt_finetune"
+CONFIG_PATH='/home/leonardosouza/certifiedGPT/configs/train_configs/certifiedgpt_finetune1.yaml'
+#CONFIG_PATH='/home/leonardosouza/certifiedGPT/configs/train_configs/cc_sbu_finetuning.yaml'
 
 # Execute the Python training script
 
-nohup python3 train.py \
+python3 train.py \
         --config-path=${CONFIG_PATH} \
         --noise_level=${NOISE_LEVEL} \
         --max_epochs=${MAX_EPOCHS} \
         --batch_size=${BATCH_SIZE} \
-        --checkpoint_name=${CHECKPOINT_NAME} > training.log 2>&1 & 
+        --checkpoint_name=${CHECKPOINT_NAME}
     
 # nohup python3 train.py \
 #         --config-path=${CONFIG_PATH} \
