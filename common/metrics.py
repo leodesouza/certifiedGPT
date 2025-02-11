65d79924f58684c5757d6d88b59f52bfc3c6ade0
Leonardo <swf_developer@outlook.com>
2025-02-11 15:00:45 -0300

    remove tensorboard

--- common/metrics.py
+++ common/metrics.py
@@ -4,5 +4,9 @@ from common.registry import registry
 
 class TPUMetrics:
     def log_tpu_metrics(self):        
-            log = f"Number of compilations: {met.metric_data('CompileTime')[:1]}"
-            xm.master_print(log)        
+        compile_time = met.metric_data('CompileTime')
+        if compile_time is not None:
+            log = f"Number of compilations: {compile_time[:1]}"
+        else:
+            log = "CompileTime metric is not available."
+        xm.master_print(log)        
