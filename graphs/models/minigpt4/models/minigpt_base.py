01d89d7a095cfdbcba482eafb883421c872d66b4
Leonardo <swf_developer@outlook.com>
2025-02-16 14:15:50 -0300

    vqa dataset image loading

--- graphs/models/minigpt4/models/minigpt_base.py
+++ graphs/models/minigpt4/models/minigpt_base.py
@@ -146,64 +146,58 @@ class MiniGPTBase(BaseModel):
         Concatenate the batched input embedding and batched output embedding together.
         Both the input and the output embedding should be right padded.
         """
+        # input_lens = []
+        # cat_embs = []
+        # cat_atts = []
+        # for i in range(input_embs.size(0)):
+        #     input_len = input_atts[i].sum()
+        #     input_lens.append(input_len)
+        #     cat_embs.append(
+        #         torch.cat([
+        #             input_embs[i][:input_len],
+        #             output_embs[i],
+        #             input_embs[i][input_len:]
+        #         ])
+        #     )
+        #     cat_atts.append(
+        #         torch.cat([
+        #             input_atts[i][:input_len],
+        #             output_atts[i],
+        #             input_atts[i][input_len:]
+        #         ])
+        #     )
+        # cat_embs = torch.stack(cat_embs)
+        # cat_atts = torch.stack(cat_atts)
+        # return cat_embs, cat_atts, input_lens
+
         input_lens = []
         cat_embs = []
         cat_atts = []
-        for i in range(input_embs.size(0)):
-            input_len = input_atts[i].sum()
+
+        input_lens = input_atts.sum(dim=1)
+        batch_size = input_embs.size(0)
+
+        for i in range(batch_size):
+            input_len = input_lens[i].item()
             input_lens.append(input_len)
-            cat_embs.append(
-                torch.cat([
+            cat_emb = torch.cat([
                     input_embs[i][:input_len],
                     output_embs[i],
                     input_embs[i][input_len:]
                 ])
-            )
-            cat_atts.append(
-                torch.cat([
+            cat_embs.append(cat_emb)
+            
+            cat_att = torch.cat([
                     input_atts[i][:input_len],
                     output_atts[i],
                     input_atts[i][input_len:]
                 ])
-            )
+            cat_atts.append(cat_att)
+            
         cat_embs = torch.stack(cat_embs)
         cat_atts = torch.stack(cat_atts)
         return cat_embs, cat_atts, input_lens
-
-        # def body(carry, x):
-        #     input_emb, input_att, output_emb, output_att = x
-        #     input_len = input_att.sum()
-
-        #     cat_emb = torch.cat([
-        #         input_emb[:input_len],
-        #         output_emb,
-        #         input_emb[input_len:]
-        #     ])
-
-        #     cat_att = torch.cat([
-        #         input_att[:input_len],
-        #         output_att,
-        #         input_att[input_len:]
-        #     ])
-
-        #     carry.append(input_len)
-        #     return carry, (cat_emb, cat_att)
-        
-        
-        # self.logger.info(f'device for concat_emb_input_output: {self.device()}')
-        # with torch_xla.runtime.xla_device():
-        #     init = torch.tensor([], requires_grad=True)
-        #     xs = (                
-        #         input_embs.to(self.device()), 
-        #         input_atts.to(self.device()), 
-        #         output_embs.to(self.device()), 
-        #         output_atts.to(self.device())
-        #     )
-        
-        # input_lens, (cat_emb, cat_atts) = torch_xla.scan(body, init, xs)
-        # torch_xla.sync()
-
-        # return torch.stack(cat_emb), torch.stack(cat_atts), input_lens
+   
 
         
     
