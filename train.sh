32acd594caba3aeee46da2b56d55e4252fe59878
Leonardo <swf_developer@outlook.com>
2025-02-17 19:50:12 -0300

    update metric method

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@ export PJRT_DEVICE=TPU
 # CHECKPOINT_NAME="vqav2_finetuning.yaml"
 # CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/train_configs/certifiedgpt_finetune1.yaml'
 
-CHECKPOINT_NAME="finetuned_minigpt4_7b_stage2_0.pth"
+CHECKPOINT_NAME="finetuned_minigpt4_7b_stage2_0_.pth"
 CONFIG_PATH='/home/leonardosouza/certifiedGPT/configs/train_configs/cc_sbu_finetuning.yaml'
 
 # Execute the Python training script
