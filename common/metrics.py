fe9a35b39d646c97f1da88177b6b12fdbbdeeebb
Leonardo <swf_developer@outlook.com>
2025-02-18 10:17:55 -0300

    yml cc_sbu_align

--- common/metrics.py
+++ common/metrics.py
@@ -10,7 +10,9 @@ class TPUMetrics:
     def __init__(self):
          self.config = registry.get_configuration_class("configuration")
     
-    def log_tpu_metrics(self, epoch, step):        
+    def log_tpu_metrics(self, epoch, step):  
+
+       xm.master_print(f" --> log_tpu_metrics")      
        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
        compile_time = met.metric_data('CompileTime')
@@ -50,12 +52,14 @@ class TPUMetrics:
        
        path = self.config.run.output_dir
        file_and_path = os.path.join(path, f'{self.config.run.checkpoint_name}.txt')
+       xm.master_print(f"file_and_path {file_and_path}")   
        os.makedirs(path, exist_ok=True)  
     
        if not os.path.exists(file_and_path):
           with open(file_and_path, 'w') as f:
             pass  
-        
+       
+       xm.master_print(f"abrindo arquivo")  
        with open(file_and_path, 'a') as file:
            file.write(log_message)
            
