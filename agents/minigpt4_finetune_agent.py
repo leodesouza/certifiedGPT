34b28aa39ad8f312dc22979f9f05c115e2d6c9b5
Leonardo <swf_developer@outlook.com>
2025-02-20 19:40:19 -0300

    lr schedule changes

--- agents/minigpt4_finetune_agent.py
+++ agents/minigpt4_finetune_agent.py
@@ -172,7 +172,7 @@ class MiniGPT4FineTuneAgent(BaseAgent):
             xm.mark_step()       
 
             step_loss = loss.detach()
-            if xm.is_master_ordinal() and step % 5 == 0:
+            if xm.is_master_ordinal() and step % 1 == 0:
                 self._tpu_metrics.log_tpu_metrics(epoch, step, step_loss, lr)   
 
             running_loss += step_loss
