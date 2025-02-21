fb93a3902d627eb8c58829e702a1199a6051168a
Leonardo <swf_developer@outlook.com>
2025-02-21 17:00:13 -0300

    wandb variable name

--- agents/minigpt4_finetune_agent.py
+++ agents/minigpt4_finetune_agent.py
@@ -172,7 +172,7 @@ class MiniGPT4FineTuneAgent(BaseAgent):
             xm.mark_step()       
 
             step_loss = loss.detach()
-            if xm.is_master_ordinal() and step % 1 == 0:
+            if xm.is_master_ordinal() and step % 10 == 0:
                 self._tpu_metrics.log_tpu_metrics("Train", epoch, step, step_loss, lr)   
 
             running_loss += step_loss
