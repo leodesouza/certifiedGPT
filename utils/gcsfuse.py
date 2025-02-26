2f5c94be29b80ac85e3c91f109fa522b6305701b
Leonardo <swf_developer@outlook.com>
2025-02-26 10:50:24 -0300

    gcsfuse package

--- utils/gcsfuse.py
+++ utils/gcsfuse.py
@@ -1,14 +1,12 @@
 import torch_xla.core.xla_model as xm
 import subprocess
-from common.registry import registry
 
-def mount_gcsfuse(bucket_name="certifiedgpt_storage", mount_point="~/storage"):
-    logger = registry.get_configuration_class("logger")
+def mount_gcsfuse(bucket_name="certifiedgpt_storage", mount_point="~/storage"):    
     try:
         subprocess.run(["gcsfuse", bucket_name, mount_point], check=True)
         xm.master_print(f"Successfully mounted {bucket_name} at {mount_point}")        
     except subprocess.CalledProcessError as e:        
-        logger.error(f"Error mounting {bucket_name}: {e}", exc_info=True)
+        pass
 
 # Run the function
 
