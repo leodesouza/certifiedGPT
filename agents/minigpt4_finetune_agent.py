565658a0f90b8c25f913b758394731563a6901fe
Leonardo <swf_developer@outlook.com>
2025-02-28 19:28:45 -0300

    added a checkpoint_save with optimizer state

--- agents/minigpt4_finetune_agent.py
+++ agents/minigpt4_finetune_agent.py
@@ -363,13 +363,13 @@ class MiniGPT4FineTuneAgent(BaseAgent):
         model = model_type.from_config(self.config.model)
         self._model = model
         
-        start_epoch, start_step = self.load_checkpoint(self._model, self.optimizer)
-        if self.start_epoch > 0:
-            self.start_epoch = start_epoch                
-        self.start_step = start_step
+        # start_epoch, start_step = self.load_checkpoint(self._model, self.optimizer)
+        # if self.start_epoch > 0:
+        #     self.start_epoch = start_epoch                
+        # self.start_step = start_step
         
         self._model.to(self.device)    
-        
+
         return self._model
     
     def save_checkpoint_with_optim(self, model, optimizer, epoch, step):        
