dda52389095fdc4fce9d385fe6028fdd9d7b0522
Leonardo <swf_developer@outlook.com>
2025-03-09 20:32:40 -0300

    prepare finetuning with noize 0 with new params values

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 
 export PJRT_DEVICE=TPU
 
-CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/train_configs/vqav2_finetuning_noise_0.5.yaml'
+CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/train_configs/vqav2_finetuning_noise_0.yaml'
 
 python3 train.py \
         --config-path=${CONFIG_PATH}        
