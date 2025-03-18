3cb3c8ddcb1db044699572345eb6068a0006bf4f
Leonardo <swf_developer@outlook.com>
2025-03-17 23:43:31 -0300

    adding bert_score eval

--- datasets/builders/base_dataset_builder.py
+++ datasets/builders/base_dataset_builder.py
@@ -74,8 +74,8 @@ class BaseDatasetBuilder:
                 else self.text_processor["eval"]
             )
 
-            questions_path = Path(questions_info.get(dataset_info).path[0])
-            annotation_paths = Path(annotations_info.get(dataset_info).path[0])
+            questions_path = questions_info.get(dataset_info).path
+            annotation_paths = annotations_info.get(dataset_info).path
             vis_paths = Path(images_info.get(dataset_info).path[0])
 
             print(f"split {dataset_info} -- questions_path: {questions_path}")
