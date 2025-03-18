7e5cebd1aca07cc048db026106f2f7309e21bd4e
Leonardo <swf_developer@outlook.com>
2025-03-17 21:39:44 -0300

    adding bert_score eval

--- launch.sh
+++ launch.sh
@@ -2,6 +2,6 @@
 
 export PJRT_DEVICE=TPU
 
-CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/eval_configs/vqav2_eval_noise_0.yaml'
+CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/eval_configs/vqav2_finetuning_noise_0.yaml'
 
-python3 launch.py eval --config-path=${CONFIG_PATH}        
+python3 launch.py train --config-path=${CONFIG_PATH}        
