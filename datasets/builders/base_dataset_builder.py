ce972d311dc99df45ab088d35e77bdeeb95ed135
Leonardo <swf_developer@outlook.com>
2025-03-21 15:29:53 -0300

    fix text_processor creation with correct eval key

--- datasets/builders/base_dataset_builder.py
+++ datasets/builders/base_dataset_builder.py
@@ -120,11 +120,13 @@ class BaseDatasetBuilder:
         vis_processor_class = registry.get_processor_class(vis_val_config.name)
         self.logger.info("Building visual processor")
         self.vis_processor["eval"] = vis_processor_class.from_config(vis_val_config)
+        self.logger.info(f"Eval visual processor: {self.vis_processor["eval"].__class__.__name__}")
 
         text_processor_class = registry.get_processor_class(text_val_config.name)
 
         self.logger.info("Building textual processor")
         self.text_processor["eval"] = text_processor_class.from_config(text_val_config)
+        self.logger.info(f"Eval text processor: {self.text_processor["eval"].__class__.__name__}")
 
     def default_config_path(self, key="default"):
         return utils.get_abs_path(self.DATASET_CONFIG_DICT[key])
