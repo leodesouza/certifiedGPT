04bbc292d8f44af7dafc325ca06b861bdab74e51
leonardosouza <leo.desouza@gmail.com>
2025-03-24 17:30:34 -0300

    initial impl of certify

--- launch.sh
+++ launch.sh
@@ -2,6 +2,6 @@
 
 export PJRT_DEVICE=TPU
 
-CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/train_configs/vqav2_finetuning_noise_0.yaml'
+CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/certify_configs/vqav2_certify_noise_0.25.yaml'
 
 python3 launch.py train --config-path=${CONFIG_PATH}        
