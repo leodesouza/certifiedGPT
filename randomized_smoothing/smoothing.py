28bdbb68bed80866b620d429b6eebbb27d630edb
leonardosouza <leo.desouza@gmail.com>
2025-03-24 20:48:08 -0300

    initial impl of certify

--- randomized_smoothing/smoothing.py
+++ randomized_smoothing/smoothing.py
@@ -136,8 +136,18 @@ class Smooth(object):
                     outputs = self.base_decoder(batch_sample)
                     logits = outputs.logits
                 xm.mark_step()
-                xm.master_print(f"output: {outputs}")
-                xm.master_print("softmax")
+
+                answers = []
+                for output_token in outputs[0]:
+                    if output_token[0] == 0:
+                        output_token = output_token[1:]
+                    output_texts = self.base_decoder.llama_tokenizer.decode(output_token, skip_special_tokens=True)
+                    output_texts = output_texts.split('</s>')[0]  # remove the stop sign </s>
+                    output_texts = output_texts.replace("<s>", "")
+                    output_texts = output_texts.split(r'[/INST]')[-1].strip()
+                    answers.append(output_texts)
+
+                xm.master_print(f"answers: {answers}")
                 probs = torch.softmax(logits, dim=-1)
                 xm.master_print("calc probs and asign to counts")
                 counts += probs.cpu().numpy().sum(axis=0)
