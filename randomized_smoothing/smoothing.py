a3202d559b4b09b1a0fe08a9f96614a08b54cf79
leonardosouza <leo.desouza@gmail.com>
2025-03-25 16:46:33 -0300

    certify tests

--- randomized_smoothing/smoothing.py
+++ randomized_smoothing/smoothing.py
@@ -137,7 +137,8 @@ class Smooth(object):
                 # batch_answers = answers * this_batch_size
                 # batch_sample["answer"] = batch_answers
 
-                texts = self.prepare_texts(question, conv_temp)
+                texts = self.prepare_texts(question[0], conv_temp)
+                xm.master_print(f"Text: {texts}")
                 predictions = []
                 xm.master_print("passing batch_sample to model (forward)")
                 max_tokens = self.config.run.max_new_tokens
