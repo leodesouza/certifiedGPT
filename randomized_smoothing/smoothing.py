607fef44305e6f97ee834f80b7a07742b7a7804e
Leonardo <swf_developer@outlook.com>
2025-03-28 13:11:08 -0300

    fix probs computing

--- randomized_smoothing/smoothing.py
+++ randomized_smoothing/smoothing.py
@@ -52,6 +52,7 @@ class Smooth(object):
         xm.master_print("draw samples of f(x+ epsilon)")
         # draw samples of f(x+ epsilon)
         counts_selection = self._sample_noise(x, n0, batch_size)        
+        xm.master_print(f"Sigma:{self.sigma}")
         xm.master_print(f"Printing counts_selection:{counts_selection}")
         raise Exception("terminou")        
         # use these samples to take a guess at the top class
