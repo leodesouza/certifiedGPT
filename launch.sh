e43d771ee9a296555582ff7ed963b654c4febded
Leonardo <swf_developer@outlook.com>
2025-04-02 10:12:16 -0300

    preparing to 0.5 and 1.0 finetuning

--- launch.sh
+++ launch.sh
@@ -2,6 +2,6 @@
 
 export PJRT_DEVICE=TPU
 
-CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/train_configs/vqav2_finetuning_noise_0.25.yaml'
+CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/train_configs/vqav2_finetuning_noise_5.0.yaml'
 
 python3 launch.py train --config-path=${CONFIG_PATH}        
