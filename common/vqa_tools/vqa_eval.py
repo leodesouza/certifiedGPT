3a8cf1f744f83d117f9828ddba14f8035145a402
Leonardo <swf_developer@outlook.com>
2025-04-17 21:55:57 -0300

    vqa accuracy

--- common/vqa_tools/vqa_eval.py
+++ common/vqa_tools/vqa_eval.py
@@ -70,7 +70,7 @@ class VQAEval:
                 acc_per_question_number[idx] = acc
 
             if answer_type == "other":
-                acc_per_question_other[idx] = self.compute_soft_accuracy(norm_pred, gt_answers)
+                acc_per_question_other[idx] = acc #self.compute_soft_accuracy(norm_pred, gt_answers)
             
             if acc < 1.0:
                 print(f"[FAIL] pred: '{norm_pred}' vs. gts: {gt_answers}: question_id: {question_id}")
