5614fcfdb703972214e2293de70760d7afb618e5
Leonardo <swf_developer@outlook.com>
2025-04-19 19:31:20 -0300

    changes to certify method

--- common/visualize.py
+++ common/visualize.py
@@ -1,5 +1,6 @@
 import torch
-from torchvision.transforms import ToPILImage
+# from torchvision.transforms import ToPILImage
+from torchvision.transforms.functional import to_pil_image
 
 import torch_xla
 import torch_xla.core.xla_model as xm
@@ -13,13 +14,13 @@ def save_image(image, image_id, question_id, noise, output):
     
     image_id = image_id.item()
     question_id = question_id.item()
-    
+
     #original image    
-    pil = toPilImage(imagem_cpu)
+    pil = to_pil_image(imagem_cpu)
     pil.save(f"{output}/{image_id}_{question_id}.png")
     
     #noisy image
-    noisy_image = torch.clamp(imagem_cpu + noise, min=0, max=0)    
-    pil = toPilImage(noisy_image)
+    noisy_image = torch.clamp(imagem_cpu + noise, min=0, max=1)    
+    pil = to_pil_image(noisy_image)
     pil.save(f"{output}/{image_id}_{question_id}_noise_{int(noise * 100)}.png")
     
