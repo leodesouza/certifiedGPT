538894fab7931a7bc9b57eeea059187136b800dd
Leonardo <swf_developer@outlook.com>
2025-05-19 15:25:52 -0300

    Attackvlm code

--- experiments/attacks/_train_adv_img_trans.sh
+++
@@ -1,9 +0,0 @@
-# #!/bin/bash
-
-# certain hyper-parameters can be modified based on user's preference
-
-python _train_adv_img_trans.py  \
-    --batch_size 10 \
-    --num_samples 5000 \
-    --steps 100 \
-    --output "/home/swf_developer/storage/attack/minigpt4_adv/" \
