b40ab0ca794c18fe1abcac6d874798f04018c824
Leonardo <swf_developer@outlook.com>
2025-05-21 20:56:50 -0300

    image 2 txt code

--- launch.sh
+++ launch.sh
@@ -6,4 +6,6 @@ CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/attack_configs/vqav2_eval_
 # CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/predict_configs/vqav2_predict_noise_0.25_n_1000.yaml'
 
 # python3 launch.py smoothing_predict --config-path=${CONFIG_PATH}
-python3 launch.py transfer_based_attack --config-path=${CONFIG_PATH}
+# python3 launch.py transfer_based_attack --config-path=${CONFIG_PATH}
+python3 launch.py img_t2_text --config-path=${CONFIG_PATH}
+
