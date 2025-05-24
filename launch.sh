4a3b3bc1d5b02f153d4dcb8a7a7e9c8a8f54cfdb
Leonardo <swf_developer@outlook.com>
2025-05-24 19:36:52 -0300

    gradio

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@
 #export PJRT_DEVICE=TPU
 # CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/attack_configs/vqav2_eval_noise_0.yaml'
 # CONFIG_PATH='/home/leonardosouza/certifiedGPT/configs/train_configs/vqav2_finetuning_noise_0.25.yaml'
-CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/predict_configs/vqav2_predict_noise_0.25_n_1000.yaml'
+CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/predict_configs/vqav2_predict_noise_0.25_n_100.yaml'
 
 python3 launch.py smoothing_predict --config-path=${CONFIG_PATH}
 # python3 launch.py transfer_based_attack --config-path=${CONFIG_PATH}
