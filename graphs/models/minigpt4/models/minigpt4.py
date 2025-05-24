1c3c202dd6237aaeae9a32cb677aed98a213d8d7
Leonardo <swf_developer@outlook.com>
2025-05-24 17:34:14 -0300

    gradio

--- graphs/models/minigpt4/models/minigpt4.py
+++ graphs/models/minigpt4/models/minigpt4.py
@@ -191,7 +191,8 @@ class MiniGPT4(MiniGPTBase):
 
         ckpt_path = "/home/swf_developer/storage/checkpoints/minigpt4_stage2/cc_sbu_finetuning_noise_0.pth" # cfg.get("ckpt", "")  # load weights of MiniGPT-4
         if ckpt_path:
-            print("Load MiniGPT-4 Checkpoint: {}".format(ckpt_path))            
+            print("Load MiniGPT-4 Checkpoint: {}".format(ckpt_path))
+            raise ValueError("Load MiniGPT-4 Checkpoint")                        
             ckpt = torch.load(ckpt_path, map_location="cpu", weights_only=False)
             msg = model.load_state_dict(ckpt['model_state_dict'], strict=False)
 
