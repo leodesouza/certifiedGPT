3b13c9e9f2f112fdf87ad7ddc3047121f452d360
Leonardo <swf_developer@outlook.com>
2025-06-04 19:07:52 -0300

    adv_img_query changes

--- experiments/attacks/_train_adv_img_query.py
+++ experiments/attacks/_train_adv_img_query.py
@@ -215,6 +215,7 @@ def main():
     query_attack_results = torch.sum(adv_vit_text_features * target_text_features, dim=1).squeeze().detach().cpu().numpy()
     print(f"query_attack_results shape: {query_attack_results.size()}")    
     print(f"query_attack_results: {query_attack_results}")    
+    raise ValueError("teste")
     assert (vit_attack_results == query_attack_results).all()
     
     ## other arch
