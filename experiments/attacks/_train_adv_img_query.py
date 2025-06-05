78068ff84d59c42d434b4b667bbcda290c9f754f
Leonardo <swf_developer@outlook.com>
2025-06-05 11:38:02 -0300

    adv_img_query changes

--- experiments/attacks/_train_adv_img_query.py
+++ experiments/attacks/_train_adv_img_query.py
@@ -345,16 +345,16 @@ def main():
             
             # computes a projection coefficient and measures how much the movement from perturb_text_features to adv_text_features aligns with tgt_text_features.             
             coefficient = torch.sum((perturb_text_features - adv_text_features) * tgt_text_features, dim=-1)
-            print(f"coefficient: {coefficient}")
+            print(f"coefficient: {coefficient.size()}")
             coefficient = coefficient.reshape(num_query, batch_size, 1, 1, 1)
-            print(f"coefficient reshape: {coefficient}")
+            print(f"coefficient reshape: {coefficient.size()}")
 
-            print(f"query_noise : {query_noise}")
+            print(f"query_noise : {query_noise.size()}")
             query_noise = query_noise.reshape(num_query, batch_size, 3, args.input_res, args.input_res)
-            print(f"query_noise reshape : {query_noise}")
+            print(f"query_noise reshape : {query_noise.size()}")
 
             pseudo_gradient = coefficient * query_noise / sigma 
-            print(f"pseudo_gradient : {pseudo_gradient}")
+            print(f"pseudo_gradient : {pseudo_gradient.size()}")
             pseudo_gradient = pseudo_gradient.mean(0) 
             
             # step 3. log metrics
