0b7dc4a85870918f7327c02cd88643f081fdc44a
Leonardo <swf_developer@outlook.com>
2025-06-12 18:40:57 -0300

    adv_img_query changes

--- randomized_smoothing/smoothing.py
+++ randomized_smoothing/smoothing.py
@@ -177,9 +177,9 @@ class Smooth(object):
         text1_count = sum(1 for row in sample_for_estimation if row[0] == text1)
         print(f'text1: {text1}')
 
-        sub_sample_for_estimation = np.array(sample_for_estimation[:,0] != text1)
+        sub_sample_for_estimation = sample_for_estimation(sample_for_estimation[:, 0] != text1)
         print(f'sub_sample_for_estimation: {sub_sample_for_estimation}')
-        sub_probs_selection = np.array(sub_sample_for_estimation[:,1], dtype=float)
+        sub_probs_selection = sub_sample_for_estimation[:, 1].astype(float)
         top2 = sub_probs_selection.argsort()[::-1][:1]    
 
         text2 = sub_sample_for_estimation[top2[0]][0]
