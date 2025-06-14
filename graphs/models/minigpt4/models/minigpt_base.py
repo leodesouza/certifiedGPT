fbf6c7b576300d222ff5153e1696787c16d20727
Leonardo <swf_developer@outlook.com>
2025-06-14 11:52:24 -0300

    adv_img_query changes

--- graphs/models/minigpt4/models/minigpt_base.py
+++ graphs/models/minigpt4/models/minigpt_base.py
@@ -67,9 +67,7 @@ class MiniGPTBase(BaseModel):
 
     def get_context_emb(self, prompt, img_list):                
         device = img_list[0].device
-        prompt_segs = prompt.split('<ImageHere>') 
-        print(f"prompt: {prompt}")  
-        print(f"prompt_segs: {prompt_segs}")              
+        prompt_segs = prompt.split('<ImageHere>')         
         assert len(prompt_segs) == len(img_list) + 1, "Unmatched numbers of image placeholders and images."
         seg_tokens = [
             self.llama_tokenizer(
