5061ab95b2bb4fc5b02b9bba428c1085ef5347e3
Leonardo <swf_developer@outlook.com>
2025-06-14 15:16:50 -0300

    adv_img_query changes

--- randomized_smoothing/smoothing.py
+++ randomized_smoothing/smoothing.py
@@ -56,6 +56,10 @@ class Smooth(object):
         # print(f'text1: {text1}')
 
         sub_sample_for_estimation = sample_for_estimation[sample_for_estimation[:, 0] != text1]
+        if sub_sample_for_estimation is None or len(sub_sample_for_estimation) == 0:
+            print("text1 is predominant")
+            return text1
+        
         # print(f'sub_sample_for_estimation: {sub_sample_for_estimation}')
         sub_probs_selection = sub_sample_for_estimation[:, 1].astype(float)
         top2 = sub_probs_selection.argsort()[::-1][:1]    
