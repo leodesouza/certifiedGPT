2910110cd02610b860287a0bddd6de029cecc6c5
Leonardo <swf_developer@outlook.com>
2025-09-03 12:03:53 -0300

    improvements on  smoothing_v2

--- common/metrics.py
+++ common/metrics.py
@@ -1,5 +1,3 @@
-import torch_xla.core.xla_model as xm
-import torch_xla.debug.metrics as met
 from common.registry import registry
 from datetime import datetime
 from common.registry import registry
@@ -17,34 +15,34 @@ class TPUMetrics:
 
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        compile_time = met.metric_data('CompileTime')
-        if compile_time is not None:
-            log_compile_time = f'Number of Compilations: {compile_time[:1]}'
-        else:
-            log_compile_time = "Compile metric is not available"
+        # compile_time = met.metric_data('CompileTime')
+        # if compile_time is not None:
+        #     log_compile_time = f'Number of Compilations: {compile_time[:1]}'
+        # else:
+        #     log_compile_time = "Compile metric is not available"
 
-        log_message = "\n".join([
-            f"Split: {split}",
-            f"Epoch: {epoch}",
-            f"Step: {step}",
-            f"Loss: {loss}",
-            f"Lr: {lr}",
-            f"TimeStamp: {timestamp}",
-            f"{log_compile_time}"
-        ])
+        # log_message = "\n".join([
+        #     f"Split: {split}",
+        #     f"Epoch: {epoch}",
+        #     f"Step: {step}",
+        #     f"Loss: {loss}",
+        #     f"Lr: {lr}",
+        #     f"TimeStamp: {timestamp}",
+        #     f"{log_compile_time}"
+        # ])
 
-        path = self.config.run.output_dir
-        if not os.path.exists(path):
-            mount_gcsfuse()
+        # path = self.config.run.output_dir
+        # if not os.path.exists(path):
+        #     mount_gcsfuse()
 
-        file_and_path = os.path.join(path, f'{self.config.run.checkpoint_name}.txt')
+        # file_and_path = os.path.join(path, f'{self.config.run.checkpoint_name}.txt')
 
-        if not os.path.exists(file_and_path):
-            with open(file_and_path, 'w') as f:
-                pass
+        # if not os.path.exists(file_and_path):
+        #     with open(file_and_path, 'w') as f:
+        #         pass
 
-        with open(file_and_path, 'a') as file:
-            file.write(log_message + "\n\n")
+        # with open(file_and_path, 'a') as file:
+        #     file.write(log_message + "\n\n")
 
     def log_checkpoint_saving(self, checkpoint_log, epoch):
 
