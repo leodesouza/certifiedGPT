9f1ba622d22cd362cfbf1756e2b4ee7ad80fdcb1
Leonardo <swf_developer@outlook.com>
2025-09-05 14:43:33 -0300

    migrate prediction from TPU to GPU

--- launch.sh
+++ launch.sh
@@ -5,7 +5,8 @@
 # CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/certify_configs/vqav2_certify_noise_0.25.yaml'
 # CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/certify_configs/vqav2_certify_noise_1.0.yaml'
 # CONFIG_PATH='/home/leonardosouza/certifiedGPT/configs/train_configs/vqav2_finetuning_noise_0.25.yaml'
-CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/predict_configs/vqav2_predict_noise_0.25.yaml'
+# CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/predict_configs/vqav2_predict_noise_0.25.yaml'
+CONFIG_PATH='/home/swf_developer/certifiedGPT/configs/predict_configs/vqav2_predict_noise_0.5.yaml'
 
 
 python3 launch.py smoothing_predict --config-path=${CONFIG_PATH}
